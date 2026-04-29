a42ef73438c078f40ce3537faf548381db8ae798
Imtiyaz Ahamad <imtiyazahamad703@gmail.com>
2026-04-29 14:25:00 +0530

    feat: assemble all components into main App layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,35 @@
-import React from 'react';\n\nconst App = () => {\n  return (\n    <div className="bg-[#050414] text-white min-h-screen">\n      <h1>Imtiyaz Portfolio</h1>\n    </div>\n  );\n};\n\nexport default App;
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import About from "./components/About/About";
+import Skills from "./components/Skills/Skills";
+import Experience from "./components/Experience/Experience";
+import Work from "./components/Work/Work";
+import Education from "./components/Education/Education";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
+import BlurBlob from './components/BlurBlob';
+
+const App = () => {
+  return (
+    <div className="bg-[#050414]">
+       {/* Removed BlurBlob from behind the text as requested */}
+       {/* <BlurBlob position={{ top: '35%', left: '20%' }} size={{ width: '30%', height: '40%' }} /> */}
+
+      <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
+      
+      <div className="relative pt-20">
+        <Navbar />
+        <About />
+        <Experience />
+        <Work />
+        <Skills />
+        <Education />
+        <Contact />
+        <Footer />
+      </div>
+
+    </div>
+  );
+};
+
+export default App;
